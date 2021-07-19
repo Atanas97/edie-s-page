ebda5ef141a5ff0902ba50c8d68572418f097184
Atanas97 <atanas19.97@abv.bg>
2021-07-20 01:10:57 +0300

    Added footer and fixed minor issues

---
+++ js/app.js
@@ -0,0 +1,24 @@
+//Mobile navigation
+const body = document.getElementsByTagName('body')[0]
+const navBtn = document.querySelector('.nav-toggle')
+const navMenu = document.querySelector('.nav-list')
+const menuBars = document.querySelector('.mobile-nav')
+const navElement = navMenu.querySelectorAll('li')
+console.log(navElement)
+
+
+console.log(body)
+
+navBtn.addEventListener('click', () => {
+    navMenu.classList.toggle('open')
+    menuBars.classList.toggle('active')
+
+    if (navMenu.classList.contains('open')) {
+        body.style.overflow = "hidden"
+        navElement.classList.add('effect')
+    } else {
+        body.style.overflow = "scroll"
+    }
+
+    navElement.classList.add('open')
+})
